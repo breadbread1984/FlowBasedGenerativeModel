7278a893e5a211545ff48e12a10a439a16b1984f
解易_Xieyi <yi.xie@enflame.cn>
2018-12-23 17:13:21 +0800

    modified:   LeakyReLU.py 	modified:   Model.py

--- Model.py
+++ Model.py
@@ -6,6 +6,7 @@ import tensorflow_probability as tfp;
 from LeakyReLU import LeakyReLU;
 
 class Model(tfp.bijectors.Bijector):
+
 	def __init__(self, num_layers = 6, validate_args = False, name = "model"):
 		super(Model, self).__init__(forward_min_event_ndims = 1, validate_args = validate_args, name = name);
 		layers = [];
@@ -19,9 +20,12 @@ class Model(tfp.bijectors.Bijector):
 			alpha = tf.Variable(0.1,dtype = tf.float32);
 			layers.append(LeakyReLU(alpha = alpha));
 		self.flow = tfp.bijectors.Chain(list(reversed(layers)));
+
 	def _forward(self,x):
 		return self.flow.forward(x);
+
 	def _inverse(self,y):
 		return self.flow.inverse(y);
+
 	def _inverse_log_det_jacobian(self,y):
 		return self.flow.inverse_log_det_jacobian(y, event_ndims = 1);
