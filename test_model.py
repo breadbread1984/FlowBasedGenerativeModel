edfcfd602faef32f4c821068687b180139ccfd00
breadbread1984 <breadbread1984@163.com>
2018-12-23 18:57:06 +0800

    修改：     test_model.py

--- test_model.py
+++ test_model.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
 
+import cv2;
+import numpy as np;
 import tensorflow as tf;
 import tensorflow_probability as tfp;
 from train_model import model_fn;
 
 def main():
+    #sample
     generator = tf.estimator.Estimator(model_fn = model_fn, model_dir = "generator_model");
     prediction = generator.predict(lambda:0);
-    for i in range(200):
-        print(next(prediction));
+    samples = [];
+    for i in range(2000):
+        samples.append(next(prediction));
+    #display
+    x,y,w,h = cv2.boundingRect(np.array(samples));
+    print(x,y,w,h);
+    img = np.zeros([240,320,3],dtype = np.uint8);
+    for sample in samples:
+        pos = (
+            int((sample[0] - x) * 320 / w),
+            int((sample[1] - y) * 240 / h)
+        );
+        cv2.circle(img,pos,1,(255,255,0));
+    cv2.namedWindow("distribution");
+    cv2.imshow("distribution",img);
+    cv2.waitKey();
 
 if __name__ == "__main__":
     main();
