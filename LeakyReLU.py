39177b18e307e628d6ab0d198b588b4ba5cd31a0
解易_Xieyi <yi.xie@enflame.cn>
2018-12-23 16:12:08 +0800

    new file:   LeakyReLU.py 	new file:   Model.py 	new file:   train_model.py

---
+++ LeakyReLU.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import numpy as np;
+import tensorflow as tf;
+import tensorflow_probability as tfp;
+
+class LeakyReLU(tfp.bijectors.Bijector):
+	def __init__(self, alpha = 0.5, validate_args = False, name = "leaky_relu"):
+		super(LeakyReLU, self).__init__(forward_min_event_ndims = 1, validate_args = validate_args, name = name);
+		self.alpha = alpha;
+	def _forward(self, x):
+		assert(x.shape[1] == 2);
+		#	  / x (x >= 0)
+		# y = |
+		#	  \ alpha * x (x < 0)
+		return tf.where(tf.greater_equal(x, 0), x, self.alpha * x);
+	def _inverse(self, y):
+		assert(y.shape[1] == 2);
+		#	  / y (y >= 0)
+		# x = |
+		#	  \ y / alpha (y < 0)
+		return tf.where(tf.greater_equal(y, 0), y, 1. / self.alpha * y);
+	def _inverse_log_det_jacobian(self, y):
+		assert(y.shape[1] == 2);
+		#			/ 1 (x >= 0)
+		# dy / dx = |
+		#			\ alpha (x < 0)
+		#						   / log(1) (x >= 0)
+		# log(inv（det(dy / dx)）) = |
+		#						   \ log(1 / alpha) (x < 0)
+		I = tf.ones_like(y);
+		J_inv = tf.where(tf.greater_equal(y, 0), I, 1.0 / self.alpha * I);
+		# sum over diagonal elements
+		return tf.reduce_sum(tf.log(tf.abs(J_inv)),axis = -1);
---
+++ Model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import numpy as np
+import tensorflow as tf;
+import tensorflow_probability as tfp;
+from LeakyReLU import LeakyReLU;
+
+class Model(tfp.bijectors.Bijector):
+	def __init__(self, num_layers = 6, validate_args = False, name = "model"):
+		super(Model, self).__init__(forward_min_event_ndims = 1, validate_args = validate_args, name = name);
+		layers = [];
+		for i in range(num_layers):
+			#affine(linear transform)
+			L = tf.Variable(np.tril(np.random.randn(2,2)), dtype = tf.float32);
+			V = tf.Variable(np.random.randn(2,2), dtype = tf.float32);
+			shift = tf.Variable(np.random.randn(2), dtype = tf.float32);
+			layers.append(tfp.bijectors.Affine(scale_tril = L, scale_perturb_factor = V, shift = shift));
+			#leakyrelu(non-linear transform)
+			alpha = tf.Variable(0.1,dtype = tf.float32);
+			layers.append(LeakyReLU(alpha = alpha));
+		self.flow = tfp.bijectors.Chain(list(reversed(layers)));
+	def _forward(self,x):
+		return self.flow.forward(x);
+	def _inverse(self,y):
+		return self.flow.inverse(y);
+	def _inverse_log_det_jacobian(self,y):
+		return self.flow.inverse_log_det_jacobian(y, event_ndims = 1);
